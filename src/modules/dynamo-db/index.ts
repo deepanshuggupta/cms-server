382d144e5d36c1720a5a5d60ff50451de32998f3
Deepanshu Gupta <deepanshu.ggupta@gmail.com>
2023-06-25 00:53:34 +0530

    Update index.ts

--- src/modules/dynamo-db/index.ts
+++ src/modules/dynamo-db/index.ts
@@ -7,7 +7,7 @@ AWS.config.update({
 export const DynamoDB = new AWS.DynamoDB.DocumentClient({
   endpoint: "http://dynamodb.us-east-1.amazonaws.com",
   credentials: {
-    accessKeyId: "AKIAREIPNZJEXH3MAHPG",
-    secretAccessKey: "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",
+    accessKeyId: "",
+    secretAccessKey: "",
   },
 });
